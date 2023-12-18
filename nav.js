df57dd067546339818239d86b318cee2998d9e5b
ymaprameya1~ <ymaprameya@gmail.com>
2023-12-18 20:14:01 +0530

    using hamburger menu

--- nav.js
+++ nav.js
@@ -1,12 +1,11 @@
-
-
   function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("sidenav").style.display='block'
+    document.getElementById("main").style.marginBottom = "250px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("Sidenav").style.width = "15px";
     document.getElementById("main").style.marginLeft = "0px";
   }
